8f972377e20a7bf78e16116f677893164bae97a4
KazaKazan <ahmetfiratusta@hotmail.com>
2023-05-15 14:53:14 +0300

    Add image export, finalize v1

--- script.js
+++ script.js
@@ -469,6 +469,49 @@ const ColorGenerator = (() => {
 
 })();
 
+const ImageGenerator = (() => {
+    const canvas = document.getElementById("saveCanvas");
+
+    function _download(){
+        const dataUrl = canvas.toDataURL();
+        const anchor = document.createElement("a");
+        anchor.href = dataUrl;
+        anchor.download = "palette.png";
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+    }
+
+    function _fillCanvas(){
+        const colors = ColorGenerator.getPalette(true);
+        const ctx = canvas.getContext("2d");
+
+        ctx.canvas.width = 900;
+        ctx.canvas.height = 150 * (Math.ceil(colors.length / 6));
+        
+        let x = 0;
+        let y = 0;
+        colors.forEach(color => {
+            ctx.fillStyle = color.hex();
+            ctx.fillRect(x, y, 150, 150);
+            x += 150;
+            if(x >= 900){
+                x = 0;
+                y += 150;
+            }
+        });
+    }
+
+    function generate(){
+        _fillCanvas();
+        _download();
+    }
+
+    return{
+        generate
+    }
+})();
+
 const DOMHandler = (() => {
 
     const swatchDisplay = document.getElementById("display");
@@ -621,6 +664,8 @@ const DOMHandler = (() => {
             
             notificationText.innerHTML = "<b>Your code is: </b>" + code;
             notificationContainer.classList.add("show")
+        }else if(exImage.checked){
+            ImageGenerator.generate();
         };
     }
 

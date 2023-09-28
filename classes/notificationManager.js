5667fbeb820ffb7e69a8514f8b5dee5e1a049bbc
KazaKazan <ahmetfiratusta@hotmail.com>
2023-09-28 10:00:09 +0300

    Add initial Notification Manager code.

---
+++ classes/notificationManager.js
@@ -0,0 +1,20 @@
+class NotificationManager{
+    constructor(parent){
+        this.parent = parent;
+        this.lastNotification = null;
+    }
+
+    _createNotification(){
+
+    }
+
+    push(){
+        
+    }
+
+    dismiss(){
+
+    }
+}
+
+export{NotificationManager}

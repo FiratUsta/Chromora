81389d3f70d0c0e9ee4b4dd050ba46e0dc25bcc7
PrimaryCat <ahmetfiratusta@hotmail.com>
2024-10-31 12:28:00 +0300

    Decouple swatch label creation and setting content logic.

--- src/classes/swatch.js
+++ src/classes/swatch.js
@@ -3,6 +3,7 @@ class Swatch{
         this.parent = parent;
         this.focused = false;
         this.locked = false;
+        this.edited = false;
 
         this.element = document.createElement("div");
         this.element.classList.add("swatch");
@@ -11,6 +12,7 @@ class Swatch{
         
         this.color = color;
         this.textColor = textColor;
+        this.labels = [];
         this.update(this.color);
     }
 
@@ -36,19 +38,19 @@ class Swatch{
     createLabels(){
         const hexLabel = document.createElement("p");
         hexLabel.classList.add("hexLabel");
-        hexLabel.innerText = this.color.hex();
         hexLabel.onclick = () => this._copyInformation("hex");
 
         const rgbLabel = document.createElement("p");
         rgbLabel.classList.add("rgbLabel");
-        rgbLabel.innerText = `rgb(${this.color.red}, ${this.color.green}, ${this.color.blue})`;
         rgbLabel.onclick = () => this._copyInformation("rgb");
 
         const hsvLabel = document.createElement("p");
-        const hsv = this.color.hsv();
         hsvLabel.classList.add("hsvLabel");
-        hsvLabel.innerText = `hsv(${parseInt(hsv.hue)}°, ${parseInt(hsv.saturation * 100)}%, ${parseInt(hsv.value * 100)}%)`;
         hsvLabel.onclick = () => this._copyInformation("hsv");
+
+        const nameLabel = document.createElement("p");
+        nameLabel.classList.add("nameLabel");
+        nameLabel.onclick = () => this._copyInformation("name");
 
         const lockLabel = document.createElement("p");
         lockLabel.classList.add("lockLabel");
@@ -58,26 +60,32 @@ class Swatch{
             }
         };
 
-        const labels = [hexLabel, rgbLabel, hsvLabel, lockLabel];
+        this.labels = [hexLabel, rgbLabel, hsvLabel, lockLabel, nameLabel];
 
-        if(this.color.name !== undefined){
-            const nameLabel = document.createElement("p");
-            nameLabel.classList.add("nameLabel");
-            nameLabel.innerText = this.color.name;
-            nameLabel.onclick = () => this._copyInformation("name");
-            labels.push(nameLabel);
-        };
-
-        labels.forEach(label => {
+        this.labels.forEach(label => {
             label.classList.add("swatchLabel");
             label.style.color = this.textColor;
             this.element.appendChild(label);
         })
+
+        this.updateLabels();
+    }
+
+    updateLabels(){
+        const hsv = this.color.hsv();
+
+        this.labels[0].innerText = this.color.hex();
+        this.labels[1].innerText = `rgb(${this.color.red}, ${this.color.green}, ${this.color.blue})`;
+        this.labels[2].innerText = `hsv(${parseInt(hsv.hue)}°, ${parseInt(hsv.saturation * 100)}%, ${parseInt(hsv.value * 100)}%)`;
+        if(this.color.name !== undefined){
+            this.labels[4].innerText = this.color.name;
+        };
     }
 
     update(color){
         this.color = color;
         this.element.style.backgroundColor = this.color.hex();
+        this.updateLabels();
     }
 }
 
